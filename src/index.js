5c6ac1bd3849b8ba46867029dfd3364feac47b9e
Joachim Toft <c.joachim.toft@gmail.com>
2018-10-31 19:02:32 +0100

    use hashrouter to make routing work with gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import 'normalize.css';
 import './global.css';
@@ -9,7 +9,7 @@ import App from './App';
 
 ReactDOM.render(
     (
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     ), document.getElementById('root'));
